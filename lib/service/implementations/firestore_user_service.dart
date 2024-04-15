a2494c683713572fa90782c0c2ab26d88181c867
Krist1e <kris.lex1e3@gmail.com>
2024-04-15 14:37:19 +0300

    Favorite cafes count added

--- lib/service/implementations/firestore_user_service.dart
+++ lib/service/implementations/firestore_user_service.dart
@@ -1,3 +1,4 @@
+import 'package:cafetopia_flutter/utils/stream_extensions.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../models/user.dart';
@@ -9,14 +10,16 @@ class FirestoreUserService implements UserService {
   FirestoreUserService(this.firestore);
 
   @override
-  Stream<User> user(String userId) {
-    return _userSnapshots(userId).map((snapshot) {
-      if (!snapshot.exists) {
-        throw StateError('User not found');
-      }
-      return User.fromMap(snapshot.data()!, snapshot.id, 0);
-    });
-  }
+  Stream<User> user(String userId) => StreamExtensions.combine(
+              [_userSnapshots(userId), _favoriteCafesCount(userId)])
+          .map((snapshots) {
+        final userSnapshot =
+            snapshots[0] as DocumentSnapshot<Map<String, dynamic>>;
+        final favoriteCafesCount = snapshots[1] as int;
+        final user = User.fromMap(
+            userSnapshot.data()!, userSnapshot.id, favoriteCafesCount);
+        return user;
+      });
 
   Stream<DocumentSnapshot<Map<String, dynamic>>> _userSnapshots(String userId) {
     return firestore.collection('users').doc(userId).snapshots();
@@ -24,30 +27,37 @@ class FirestoreUserService implements UserService {
 
   @override
   Future<void> createUserProfile(
-      {required String id, required String name, required String email}) async {
-    return await firestore.collection('users').doc(id).set({
-      'name': name,
-      'email': email,
-      'dateOfRegistration': Timestamp.fromDate(DateTime.now()),
-    });
-  }
+          {required String id,
+          required String name,
+          required String email}) async =>
+      await firestore.collection('users').doc(id).set({
+        'name': name,
+        'email': email,
+        'dateOfRegistration': Timestamp.fromDate(DateTime.now()),
+      });
 
   @override
   Future<void> updateProfile(
-      {required String id,
-        required String name,
-        required String gender,
-        required String country,
-        required String city,
-        required String bio,
-        required String favoriteFood}) async {
-    return await firestore.collection('users').doc(id).update({
-      'name': name,
-      'gender': gender,
-      'country': country,
-      'city': city,
-      'bio': bio,
-      'favoriteFood': favoriteFood,
-    });
-  }
+          {required String id,
+          required String name,
+          required String gender,
+          required String country,
+          required String city,
+          required String bio,
+          required String favoriteFood}) async =>
+      await firestore.collection('users').doc(id).update({
+        'name': name,
+        'gender': gender,
+        'country': country,
+        'city': city,
+        'bio': bio,
+        'favoriteFood': favoriteFood,
+      });
+
+  Stream<int> _favoriteCafesCount(String userId) => firestore
+      .collection('users')
+      .doc(userId)
+      .collection('favorites')
+      .snapshots()
+      .map((snapshot) => snapshot.docs.length);
 }
